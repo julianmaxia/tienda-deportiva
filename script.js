2b0a6d2f910344f313f5ace3d6f3f64f97bc3c98
Julian David Macias <julian.maxia69@gmail.com>
2026-08-31 20:55:05 -0500

    Agregar contador del carrito

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-javascript
+let carrito = 0;
+
 const botones = document.querySelectorAll(".producto button");
+const contador = document.getElementById("contador-carrito");
 
 botones.forEach(function(boton) {
     boton.addEventListener("click", function() {
+        carrito++;
+        contador.textContent = "🛒 " + carrito;
         alert("Producto agregado al carrito 🛒");
     });
 });
