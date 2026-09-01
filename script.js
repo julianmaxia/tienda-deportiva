f84bd88e19964d069be94fb3a23cd1dfa9d0967a
Julian David Macias <julian.maxia69@gmail.com>
2026-08-31 20:59:24 -0500

    Crear sección visual del carrito

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let carrito = 0;
 
 const botones = document.querySelectorAll(".producto button");
 const contador = document.getElementById("contador-carrito");
+const cantidadCarrito = document.getElementById("cantidad-carrito");
 
 botones.forEach(function(boton) {
     boton.addEventListener("click", function() {
 
         carrito++;
         contador.textContent = "🛒 " + carrito;
+        cantidadCarrito.textContent = carrito;
 
         const producto = boton.parentElement.querySelector("h3").textContent;
 
